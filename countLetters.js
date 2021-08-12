62dbd5d50560f613cc7a84f60dadd32d42aab2da
Francis Kabigting <franciskabigting@rocketmail.com>
2021-08-11 22:09:35 -0400

    coontletters

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const countLetters = function(sentence) {
+  const letterCount = {};
+  for (const letter of sentence) {
+    if (letter !== ' ') {
+      if (letterCount[letter]) {
+        letterCount[letter] += 1;
+      } else {
+        letterCount[letter] = 1;
+      }
+    }
+  }
+  return letterCount;
+};
+
+console.log(countLetters('lighthouse Labs'));
